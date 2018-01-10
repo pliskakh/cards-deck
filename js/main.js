98ac319c2e537f0d743792c790d1e40c0caa286c
Misha Pliskunov <pliskunov.mykhailo@gmail.com>
2018-01-10 11:10:21 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,9 @@
+;(function(){
+	"use strict";
+	var spades = document.querySelector('.spades');
+	// function stackUp(){
+	// 	for (var i = 6; i <= 10; i++;) {
+	// 		console.log(spades);
+	// 	}
+	// }
+})();
